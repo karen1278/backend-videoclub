05928cc9b6f027db16f029312b64d1471c1ec9a7
karen1278 <kjaneth111@gmail.com>
2023-02-15 15:14:06 -0500

    new commit

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const mongoose = require("mongoose");
-const cors = require('cors');
 
 const app = express();
 const PORT = 3030;
@@ -13,8 +12,18 @@ const connectionOptions =
   useNewUrlParser: true
 };
 
-app.use(cors()); // Agrega el middleware CORS
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
 mongoose.set('strictQuery', true);
 mongoose.connect("mongodb+srv://yochi22:Marcador22@cluster0.jzbl6wm.mongodb.net/videoclub", connectionOptions)
